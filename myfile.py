81250e5b3fe26e4dff282f46ae8512c5efaa85ae
patfb <snailmail>
2020-01-08 21:25:38 -0500

    typo in readme

--- myfile.py
+++ myfile.py
@@ -6,21 +6,20 @@
 # 2 iteration
 # muppets = ["Kermit", "Miss Piggy", "Gonzo"]
 # for i, muppet in enumerate(muppets):
-#     print("iteration {iterationPlaceholder} is {muppetPlaceholder}".format(
-#         iterationPlaceholder=i, muppetPlaceholder=muppet))
+#         print("iteration {iterationPlaceholder} is {muppetPlaceholder}".format(
+#             iterationPlaceholder=i, muppetPlaceholder=muppet))
 
 # 3 functions
 # def greet(name):
 #     print("Hello", name)
-
 # greet("Kermit")
 # greet("Miss Piggy")
 # greet("Gonzo")
 
 # 4 importing modules
-# from datetime import date
-# today = date.today()
-# print("Today is", today)
+#from datetime import date
+#today = date.today()
+#print("Today is", today)
 
 # 5 using the operating system (OS) module
 #from os import getlogin
